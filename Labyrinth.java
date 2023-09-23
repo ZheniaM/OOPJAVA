c47aa4aba97c8d0b69b7add02ec0bc9bb5afb084
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-09-23 15:58:44 +0500

    modified:   Labyrinth.java

--- Labyrinth.java
+++ Labyrinth.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class Labyrinth {
 	static public void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
+		int number;
+		System.out.print("Enter number: ");
+		while (true) {
+			try {
+				number = scan.nextInt();
+			} catch (IOException error) {
+				System.out.println("try again");
+				continue;
+			}
+			break;
+		}
 		scan.close();
 		System.out.println(number + 1);
 	}
