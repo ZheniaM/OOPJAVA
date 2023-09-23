dcd2b341317c4da2bc0d8ac5125850e3eeaa2e4f
DenisKh <79641502+Gaelleaf@users.noreply.github.com>
2023-09-23 15:05:47 +0500

    modified:   Labyrinth.java

--- Labyrinth.java
+++ Labyrinth.java
@@ -1,12 +1,19 @@
 package OOPJAVA;
 
+import java.io.Console;
 import java.util.Scanner;
 
 public class Labyrinth {
 	static public void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
+		var number = scan.next();
+		try {
+			int per = Integer.valueOf(number);
+		}
 		scan.close();
-		System.out.println(number + 1);
+		if(number.equals("-help")||number.equals("-h")){
+			System.out.println("We tried");
+		}
+		System.out.println(per + 1);
 	}
 }
