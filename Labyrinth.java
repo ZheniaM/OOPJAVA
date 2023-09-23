732ee27a3a5f9a67fead7bfec62b88b2cb1c44e4
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-09-23 14:29:25 +0500

    new file:   Labyrinth.java 	starting work

---
+++ Labyrinth.java
@@ -0,0 +1,12 @@
+package OOPJAVA;
+
+import java.util.Scanner;
+
+public class Labyrinth {
+	static public void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		scan.close();
+		System.out.println(number + 1);
+	}
+}
