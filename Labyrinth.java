74d13218835e6d5e8a8b40bc377294c79ca7f6a0
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-09-23 16:11:29 +0500

    modified:   Labyrinth.java

--- Labyrinth.java
+++ Labyrinth.java
@@ -5,32 +5,30 @@ import java.util.Scanner;
 public class Labyrinth {
 	static public void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
-		scan.close();
-		if(number.equals("-help")||number.equals("-h")){
-			System.out.println("We tried");
-		}
-		System.out.println(per + 1);
-		Scanner scan = new Scanner(System.in);
 		System.out.println("Hello World");
 		System.out.println("Choose a path");
 		String input = scan.next();
+		scan.close();
 		switch(input){
-		    case("f"):
-		        System.out.println("You went forward");
-		        break;
-		    case("r"):
-		        System.out.println("You went right");
-		        break;
-		    case("l"):
-		        System.out.println("You went left");
-		        break;
-            case("/help"):
-                System.out.println("Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.");
-		        break;
-		    default:
-		        System.out.println("There is no such direction");
-		        break;
+			case("f"):
+			case("front"):
+				System.out.println("You went forward");
+				break;
+			case("r"):
+			case("right"):
+				System.out.println("You went right");
+				break;
+			case("l"):
+			case("left"):
+				System.out.println("You went left");
+				break;
+			case("--help"):
+			case("-h"):
+				System.out.println("Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.");
+				break;
+			default:
+				System.out.println("There is no such direction");
+				break;
 		}
 	}
 }
