0f7f4be45294f0ca3795feb39cd178806f46857b
DenisKh <79641502+Gaelleaf@users.noreply.github.com>
2023-10-02 20:26:52 +0500

    modified:   Labyrinth-app/src/test/java/Labyrinth/AppTest.java modified:   Labyrinth-app/target/test-classes/Labyrinth/AppTest.class

--- Labyrinth-app/src/test/java/Labyrinth/AppTest.java
+++ Labyrinth-app/src/test/java/Labyrinth/AppTest.java
@@ -1,5 +1,7 @@
 package Labyrinth;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,7 +10,30 @@ import org.junit.Test;
  */
 public class AppTest {
 	@Test
-	public void testTrue() {
-		Assert.assertTrue(true);;
-	}
+  public void testInput() {
+    String input = "f";
+    String expectedOutput = "You went forward";
+    String actualOutput = getOutput(input);
+    assertEquals(expectedOutput, actualOutput);
+  }
+  
+  private String getOutput(String input) {
+    switch(input) {
+      case("f"):
+      case("front"):
+        return "You went forward";
+      case("r"):
+      case("right"):
+        return "You went right";
+      case("l"):
+      case("left"):
+        return "You went left";
+      case("--help"):
+      case("-h"):
+        return "Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.";
+      default:
+        return "There is no such direction";
+    }
+  }
 }
+
