5299f7af170ed6d2f07a63771fc105b41b297b77
DenisKh <79641502+Gaelleaf@users.noreply.github.com>
2023-10-08 00:41:13 +0500

    added first test

--- Labyrinth-app/src/main/java/Labyrinth/App.java
+++ Labyrinth-app/src/main/java/Labyrinth/App.java
@@ -1,4 +1,5 @@
 package Labyrinth;
+
 import java.util.Scanner;
 
 public class App {
@@ -8,22 +9,27 @@ public class App {
 		System.out.println("Choose a path");
 		String input = scan.next();
 		scan.close();
-		switch(input){
-			case("f"):
-			case("front"):
+		destination(input);
+	}
+
+	public static void destination(String input) {
+		switch (input) {
+			case ("f"):
+			case ("front"):
 				System.out.println("You went forward");
 				break;
-			case("r"):
-			case("right"):
+			case ("r"):
+			case ("right"):
 				System.out.println("You went right");
 				break;
-			case("l"):
-			case("left"):
+			case ("l"):
+			case ("left"):
 				System.out.println("You went left");
 				break;
-			case("--help"):
-			case("-h"):
-				System.out.println("Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.");
+			case ("--help"):
+			case ("-h"):
+				System.out.println(
+						"Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.");
 				break;
 			default:
 				System.out.println("There is no such direction");
--- Labyrinth-app/src/test/java/Labyrinth/AppTest.java
+++ Labyrinth-app/src/test/java/Labyrinth/AppTest.java
@@ -1,6 +1,8 @@
 package Labyrinth;
 
 import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,36 +12,22 @@ import org.junit.Test;
  */
 public class AppTest {
 	@Test
-<<<<<<< HEAD
-  public void testInput() {
-    String input = "f";
-    String expectedOutput = "You went forward";
-    String actualOutput = getOutput(input);
-    assertEquals(expectedOutput, actualOutput);
-  }
-  
-  private String getOutput(String input) {
-    switch(input) {
-      case("f"):
-      case("front"):
-        return "You went forward";
-      case("r"):
-      case("right"):
-        return "You went right";
-      case("l"):
-      case("left"):
-        return "You went left";
-      case("--help"):
-      case("-h"):
-        return "Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.";
-      default:
-        return "There is no such direction";
-    }
-  }
-=======
-	public void testTrue() {
-		Assert.assertTrue(true);
+	public void testDestination() {
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		App.destination("f");
+		assertEquals("You went forward\r\n", outContent.toString());
+		outContent.reset();
+		App.destination("r");
+		assertEquals("You went right\r\n", outContent.toString());
+		outContent.reset();
+		App.destination("l");
+		assertEquals("You went left\r\n", outContent.toString());
+		outContent.reset();
+		App.destination("-h");
+		assertEquals(
+				"Write 'l' if you want to go left, 'r' if you want to go right or 'f' if you want to go straight.\r\n",
+				outContent.toString());
+		outContent.reset();
 	}
->>>>>>> 28e49e31ba7e8bfff849459924c11935aafe2631
 }
-
