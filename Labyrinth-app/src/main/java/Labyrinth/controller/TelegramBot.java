1cfc9d1a59edd9b50c163e7b1c16848ba56ebd3e
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-10-16 19:55:00 +0500

    hide tgid

--- Labyrinth-app/src/main/java/Labyrinth/controller/TelegramBot.java
+++ Labyrinth-app/src/main/java/Labyrinth/controller/TelegramBot.java
@@ -1,5 +1,8 @@
 package Labyrinth.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -10,31 +13,43 @@ import Labyrinth.App;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
-    private App app;
-    public TelegramBot(App app){
-        this.app=app;
-    }
-    @Override
-    public String getBotUsername() {
-        return "example_demobot";
-    }
+	private App app;
 
-    @Override
-    public String getBotToken() {
-        return "6323682351:AAGFQ7MXHzVkAQ3E64glITsQxj8UZpwczcc";
-    }
+	public TelegramBot(App app) {
+		this.app = app;
+	}
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        String chatId = update.getMessage().getChatId().toString();
-        String text = update.getMessage().getText();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-        sendMessage.setText(app.destination(text));
-        try {
-            this.execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@Override
+	public String getBotUsername() {
+		return "example_demobot";
+	}
+
+	@Override
+	public String getBotToken() {
+		Scanner scanner;
+		String result;
+		try {
+			scanner = new Scanner(new File("tgid.txt"));
+			result = scanner.nextLine();
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			result = "";
+			System.out.println(e);
+		}
+		return result;
+	}
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		String chatId = update.getMessage().getChatId().toString();
+		String text = update.getMessage().getText();
+		SendMessage sendMessage = new SendMessage();
+		sendMessage.setChatId(chatId);
+		sendMessage.setText(app.destination(text));
+		try {
+			this.execute(sendMessage);
+		} catch (TelegramApiException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
