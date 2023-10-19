9c32200d16c036c1b431ee1e85b415d15828cdd3
DenisKh <79641502+Gaelleaf@users.noreply.github.com>
2023-10-19 18:57:05 +0500

    modified:   Labyrinth-app/src/main/java/Labyrinth/App.java modified:   Labyrinth-app/src/test/java/Labyrinth/AppTest.java off comms working bot

--- Labyrinth-app/src/main/java/Labyrinth/App.java
+++ Labyrinth-app/src/main/java/Labyrinth/App.java
@@ -50,24 +50,4 @@ public class App {
 	public String getHelp() {
 		return helpMessage;
 	}
-	/*
-	 * private static void mainLoop() { String input; System.out.println(this.map.show()); while
-	 * (true) { input = this.scanner.nextLine(); if (input.equals("q") || input.equals("quit")) {
-	 * break; } this.player.setDirection(input); moveCharacter(player);
-	 * System.out.println(this.map.show()); } }
-	 * 
-	 * private static void initScene() { this.scanner = new Scanner(System.in); this.player = new
-	 * Character(1, 1, 0); this.map = new Plane(new byte[][] {{1, 1, 1, 1, 1, 1, 1, 1, 1}, {1, 0, 0,
-	 * 1, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 0, 1, 0, 0, 1}, {1, 0, 1, 0, 1, 1, 0, 0, 1}, {1, 1, 1, 1, 1,
-	 * 1, 1, 1, 1},}); this.map.setCell(this.player, Plane.Type.ENTITY); }
-	 * 
-	 * private static void moveCharacter(Character character) { this.map.setCell(character,
-	 * Plane.Type.EMPTY); character.move(); if (this.map.getCell(character) == Plane.Type.WALL) {
-	 * character.returnToPreviousPoint(); } this.map.setCell(character, Plane.Type.ENTITY); }
-	 * 
-	 * public void processTelegramInput(String input) { player.setDirection(input);
-	 * moveCharacter(player); }
-	 * 
-	 * public String getMapOutput() { return map.show(); }
-	 */
 }
