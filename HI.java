5cb33a47539ade88368d7d6797dc56d16675c04a
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-10-20 17:15:09 +0500

    added make file

--- HI.java
+++
@@ -1,9 +0,0 @@
-package OOPJAVA;
-
-public class HI {
-	static public void main(String args[]) {
-		System.out.println("Hi!");
-		System.out.println("Hi!");
-		System.out.println("Hello World!");
-	}
-}
