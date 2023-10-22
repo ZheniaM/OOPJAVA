99f5300ca288c57a609d3066988672c06115a39f
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-10-22 17:03:19 +0500

    tile drawing test

--- Labyrinth-app/src/main/java/Labyrinth/controller/TelegramBot.java
+++ Labyrinth-app/src/main/java/Labyrinth/controller/TelegramBot.java
@@ -3,12 +3,28 @@ package Labyrinth.controller;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import Labyrinth.App;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
+
 	private App app;
 
 	public TelegramBot(App app) {
@@ -22,7 +38,25 @@ public class TelegramBot extends TelegramLongPollingBot {
 
 	@Override
 	public String getBotToken() {
-		return "";
+		ClassLoader classLoader = getClass().getClassLoader();
+		File fileToken = new File(classLoader.getResource("tgToken.txt").getFile());
+		try {
+			InputStream is = new FileInputStream(fileToken);
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+				String token;
+				token = br.readLine();
+				if (token != null) {
+					return token;
+				}
+				return "";
+			} catch (IOException e) {
+				e.printStackTrace();
+				return "";
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return "";
+		}
 	}
 
 	@Override
@@ -30,12 +64,42 @@ public class TelegramBot extends TelegramLongPollingBot {
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			long chatId = update.getMessage().getChatId();
 			String input = update.getMessage().getText();
+			// crate text massege
 			SendMessage message = new SendMessage();
 			message.enableHtml(true);
 			message.setChatId(String.valueOf(chatId));
 			message.setText(app.moveAndGetMapHTML(input));
+			// end of text massege
+
+			// create simple image
+			BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+			Graphics2D g2d = image.createGraphics();
+			g2d.setPaint(Color.RED);
+
+			ClassLoader classLoader = getClass().getClassLoader();
+			File fileTile = new File(classLoader.getResource("Sprite.png").getFile());
+			try {
+				Image tile = ImageIO.read(fileTile);
+				g2d.drawImage(tile, 0, 0, 16, 16, null);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			g2d.dispose();
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			try {
+				ImageIO.write(image, "png", outputStream);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			byte[] imageData = outputStream.toByteArray();
+			SendPhoto sendPhoto = new SendPhoto();
+			String id = update.getMessage().getChatId().toString();
+			sendPhoto.setChatId(id);
+			sendPhoto.setPhoto(new InputFile(new ByteArrayInputStream(imageData), "image.png"));
+			// end of image
 			try {
 				execute(message);
+				execute(sendPhoto);
 			} catch (TelegramApiException e) {
 				e.printStackTrace();
 			}
