3bd56a4a2a95f09728553db139e9da9934114f3f
ZheniaM <108816223+ZheniaM@users.noreply.github.com>
2023-11-01 14:10:58 +0500

    Cell test upgraded

--- Labyrinth-app/src/test/java/Labyrinth/CellTest.java
+++ Labyrinth-app/src/test/java/Labyrinth/CellTest.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class CellTest {
 	@Test
 	public void testSymbols() {
-		Assert.assertEquals("P", Cell.PLAYERR_FLOOR.symbol);
-		Assert.assertEquals("X", Cell.EXIT.symbol);
-		Assert.assertEquals("░", Cell.FLOOR.symbol);
-		Assert.assertEquals("█", Cell.WALL.symbol);
-		Assert.assertEquals("█", Cell.WALL_LEFT.symbol);
-		Assert.assertEquals("█", Cell.WALL_RIGHT.symbol);
-		Assert.assertEquals("█", Cell.WALL_TB.symbol);
-		Assert.assertEquals("█", Cell.WALL_BRI.symbol);
-		Assert.assertEquals("█", Cell.WALL_BLI.symbol);
-		Assert.assertEquals("█", Cell.WALL_UR.symbol);
-		Assert.assertEquals("█", Cell.WALL_UL.symbol);
+		Assert.assertEquals("P", Cell.PLAYERR_FLOOR.getSymbol());
+		Assert.assertEquals("X", Cell.EXIT.getSymbol());
+		Assert.assertEquals("░", Cell.FLOOR.getSymbol());
+		Assert.assertEquals("█", Cell.WALL.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_LEFT.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_RIGHT.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_TB.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_BRI.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_BLI.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_UR.getSymbol());
+		Assert.assertEquals("█", Cell.WALL_UL.getSymbol());
 	}
 }
